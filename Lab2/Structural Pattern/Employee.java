f4e3fc037a64d7bd4ee8305a81c2d176676da989
Olena Sheremet <lenka.sheremet1978@gmail.com>
2023-03-16 14:15:13 +0200

    Add files via upload

--- Lab2/Structural Pattern/Employee.java
+++ Lab2/Structural Pattern/Employee.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Director implements Person{
+public class Employee implements Person{
     private int id;
     private String name;
     private double salary;
     List<Person> personList=new ArrayList<Person>();
-    public Director(int id, String name,double salary){
+    public Employee(int id, String name,double salary){
         this.id=id;
         this.name=name;
         this.salary=salary;
@@ -30,20 +30,25 @@ public class Director implements Person{
     @Override
     public void print() {
         System.out.println("__________________________");
-        System.out.println( "Id =" +getId());
-        System.out.println( "Назва =" +getName());
-        System.out.println( "Зарплата =" +getSalary());
+        System.out.println("Id ="+getId());
+        System.out.println("Name ="+getName());
+        System.out.println("Salary ="+getSalary());
         System.out.println("__________________________");
+        Iterator<Person> it = personList.iterator();
+
+        while(it.hasNext())  {
+            Person person = it.next();
+            person.print();
+        }
     }
 
     @Override
     public void add(Person person) {
-
+        personList.add(person);
     }
 
     @Override
     public void remove(Person person) {
-
+        personList.remove(person);
     }
-
 }
--- Lab2/Structural Pattern/Main.java
+++ Lab2/Structural Pattern/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         //Composite Pattern
         Person person1=new Accountant(145,"Alica Jonathan", 15000.0);
-        Person person2=new Salesman(160,"Will Brookes",10500.0);
+        Person person2=new Director(160,"Will Brookes",25000.0);
         Person person3=new Salesman(161,"Jane Weller",9000.0);
         Person person4=new Salesman(163,"Sam Harris",1000.0);
-        Person employees=new Director(123,"John Brawn", 35000.0);;
+        Person employees=new Employee(123,"John Brawn", 35000.0);;
         employees.add(person1);
         employees.add(person4);
         employees.add(person3);
